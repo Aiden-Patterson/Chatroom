768c9b6391b4d4c561be68615880220eeba50c13
aiden-patterson <pat18001@byui.edu>
2022-12-06 14:38:42 -0700

    fixed comments

--- gui - Copy.py
+++ gui - Copy.py
@@ -12,15 +12,11 @@ HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 1234
 # Create a socket
-# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
-# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # Connect to a given ip and port
 client_socket.connect((IP, PORT))
-
 client_socket.setblocking(False)
-#Create all of the functions needed for the gui to function
+
 
 # send the message to the server
 def sendMessage(message):
@@ -32,10 +28,11 @@ def sendMessage(message):
 def displayMessage(message):
     msgDisplay.insert(END, message)
 
-#send the message when you click the send button
+#get the message from the textbox when you click the button, display it to the user and send it to the server
 def myClick():
     if(textBox.get() != ''):
         global USERNAME
+        # if username is empty, that means this is the first message of the program and put it as the USERNAME
         if USERNAME == "":
             USERNAME = textBox.get()
             textBox.delete(0, 'end')
@@ -45,30 +42,31 @@ def myClick():
             sendMessage(textBox.get() + "\n")
             textBox.delete(0, 'end')
 
+
 #Build the GUI
 scroller = Scrollbar(root)
 myButton = Button(root, text="Send message", padx=15, pady = 15, command=myClick, fg="white", bg="blue")
 msgDisplay = Listbox(root, width = 15, height = 15, yscrollcommand=scroller.set)
 textBox = Entry(root, width=50, border=5)
-
 scroller.config(command=msgDisplay.yview)
+
 myButton.pack(side = BOTTOM)
 scroller.pack(side = LEFT, fill = Y)
 msgDisplay.pack(side=TOP, fill=X)
 textBox.pack(side = BOTTOM)
 
+#first message will always ask for the username, program will not connect to the server until an inital message is typed
 displayMessage("Please enter your username")
 
 # listen for any messages recieved
 def getMessage():
     try:
-        # Now we want to loop over received messages (there might be more than one) and print them
+        # loop over received messages and display them
         while True:
-            #msgDisplay.insert(END, "This is the while True loop in getMessage")
             # Receive our "header" containing username length, it's size is defined and constant
             username_header = client_socket.recv(HEADER_LENGTH)
 
-            # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+            # If we received no data, server gracefully closed a connection
             if not len(username_header):
                 displayMessage('Connection closed by the server')
                 sys.exit()
@@ -79,7 +77,7 @@ def getMessage():
             # Receive and decode username
             username = client_socket.recv(username_length).decode('utf-8')
 
-            # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
+            # Receive and decode message
             message_header = client_socket.recv(HEADER_LENGTH)
             message_length = int(message_header.decode('utf-8').strip())
             message = client_socket.recv(message_length).decode('utf-8')
@@ -99,9 +97,8 @@ def getMessage():
     #keep checking for new messages
     root.after(1000, getMessage)
 
+#send the username to the server, called at the beginning after the initial message
 def sendUser():
-
-    #send the username to the server
     my_user = USERNAME.encode('utf-8')
     username_header = f"{len(USERNAME):<{HEADER_LENGTH}}".encode('utf-8')
     client_socket.send(username_header + my_user)
